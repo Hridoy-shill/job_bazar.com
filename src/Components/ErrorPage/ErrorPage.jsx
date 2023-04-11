740c25033eee33000746f67d0c68fc3dd746bef2
Hridoy-shill <ridoyaaa60@gmail.com>
2023-04-11 20:27:48 +0600

    error page added

--- src/Components/ErrorPage/ErrorPage.jsx
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Lottie from "lottie-react";
+import errorAnimation from "../../assets/5707-error.json";
+import { useRouteError } from 'react-router-dom';
 
 const ErrorPage = () => {
+    const {error} = useRouteError();
     return (
-        <div>
-            <h2>rhis is error page</h2>
+        <div className='flex flex-col items-center my-20'>
+           <p className='h-80 w-80 text-white'><Lottie animationData={errorAnimation}></Lottie></p>
+           <p className='text-9xl font-extrabold'>404</p>
+           <p className='font-semibold text-2xl'>{error.message}</p>
         </div>
     );
 };
